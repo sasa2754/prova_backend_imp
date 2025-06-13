a4e971a932fa7dc93bd559f969d6b224ab4a8489
sasa2754 <sasamor5@gmail.com>
2025-06-13 14:30:31 -0300

    mudando o bgl do banco 1

--- config/db.js
+++ config/db.js
@@ -1,10 +1,10 @@
 const mysql = require('mysql2');
 
 const connection = mysql.createConnection({
-  host: 'localhost',
+  host: 'mysql.railway.internal',
   user: 'root',
-  password: 'root',
-  database: 'aula',
+  password: 'qTgrUOpZGtouVCLcTGBkeKnOpVYnHyTb',
+  database: 'railway',
 });
 
 connection.connect(err => {
